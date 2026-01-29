aeb31d2d06c8f52bdb917b9f9f03a5136ea1fb59
GaraKasturi <garakasturi@gmail.com>
2026-01-29 09:32:55 +0530

    update vercel.json

--- client/src/components/createText.js
+++ client/src/components/createText.js
@@ -18,7 +18,7 @@ const CreateText = () => {
         },
       );
       const urlId = res.data.id;
-      const completeUrl = `https://pastebinliteapp.vercel.app/v/${urlId}`;
+      const completeUrl = `https://pastebin-lite-app-two.vercel.app/v/${urlId}`;
       setUniqueLink(completeUrl);
       console.log(res);
     } catch (err) {
